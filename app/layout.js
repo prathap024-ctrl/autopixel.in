73da294c661515dc151a94ed2520aea4870ec297
prathap024-ctrl <prathaplokesh04@gmail.com>
2025-08-17 11:23:46 +0530

    font change

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { NavbarLayout } from "@/layouts/Navbar";
 import "./globals.css";
-import { Outfit } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import FooterLayout from "@/layouts/footer";
 
 export const metadata = {
@@ -9,16 +9,16 @@ export const metadata = {
     "AutoPixel helps businesses grow with AI-powered marketing, automation, and digital solutions tailored to drive results. Scale smarter with performance-driven strategies.",
 };
 
-const outfit = Outfit({
+const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-outfit",
+  variable: "--font-space-grotesk",
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={outfit.style}>
+      <body style={spaceGrotesk.style}>
         <NavbarLayout />
         {children}
         <FooterLayout />
